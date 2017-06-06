03ebf5e181b91e22842fd826d4e0e21155dd739a
Chylli <chylli@binary.com>
2017-06-06 11:34:59 +0800

    init

---
+++ cpanfile
@@ -0,0 +1,12 @@
+requires 'indirect',    '>= 0.37';
+requires 'DBIx::Connector', '>= 0.56';
+requires 'DBD::Pg';
+
+on test => sub {
+    requires 'Test::More', '>= 0.98';
+};
+
+on develop => sub {
+    requires 'Devel::Cover', '>= 1.23';
+    requires 'Devel::Cover::Report::Codecov', '>= 0.14';
+};
---
+++ lib/DBIx/Connector/Pg.pm
@@ -0,0 +1,48 @@
+package DBIx::Connector::Pg;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+use parent 'DBIx::Connector';
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+DBIx::Connector::Pg - Module abstract
+
+=head1 SYNOPSIS
+
+    use DBIx::Connector::Pg;
+    my $instance = DBIx::Connector::Pg->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+=head1 AUTHOR
+
+Chylli <chylli@163.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Chylli.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
